a9255cdbf83a64ebc5225ab66ab2208720a75769
klajdi.hasa <klajdi.hasa@softsolution.al>
2024-12-08 00:08:54 +0100

    push ndryshime firstChanges Microservice

---
+++ src/Services/Catalog/WebApplication1/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication1.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = default!;
+        public List<string> Categpry { get; set; } = new();
+        public string Description { get; set; } = default!;
+        public string ImageFile { get; set; } = default!;
+        public decimal Price { get; set; }
+    }
+}
---
+++ src/Services/Catalog/WebApplication1/Product/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,30 @@
+﻿using BuildingBlocks.CQRS;
+using WebApplication1.Models;
+
+namespace WebApplication1.Product.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+        :ICommand<CreateProductResult>;
+
+    public record CreateProductResult(Guid Id);
+    internal class CreateProducCommandtHandler 
+     : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        //Create product entity from command object
+        //save to database
+        //return result
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            var product = new WebApplication1.Models.Product
+            {
+                Name = command.Name,
+                Categpry = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+
+            return new CreateProductResult(Guid.NewGuid());
+        }
+    }
+}
